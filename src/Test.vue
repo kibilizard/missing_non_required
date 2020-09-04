7c277584faf53fd9e507ccd0c084ab7a10d86657
kibilizard <makeeva.as.work@gmail.com>
2020-09-04 11:10:08 +0300

    test missing

--- src/Test.vue
+++ src/Test.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
     {{ fo }}
+    <TestComponent />
   </div>
 </template>
 
 <script>
+import TestComponent from './test_component.vue'
 import * as _ from 'lodash'
+import Vue from 'vue'
 
-export default {
-  props: ['foo', 'bar']
-}
+export default Vue.extend({
+  components: {
+    TestComponent
+  }
+    
+})
 </script>
---
+++ src/test_component.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>TEST</div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+
+interface IProps {
+    type: string,
+    test: string
+}
+export default Vue.extend<{}, {}, {}, IProps>({
+    name: 'TestComponent',
+    props: {
+        type: String,
+        test: String
+    }
+    
+})
+</script>
